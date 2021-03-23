804e82d89fff5779482cba846d44eeb982efeba7
lnmunoz <luchomunozm90@gmail.com>
2021-03-23 20:10:15 -0300

    New files added

--- CodeLambdaResponseWell.py
+++ CodeLambdaResponseWell.py
@@ -1,22 +1,28 @@
+from botocore.vendored import requests
 import json
 
-def lambda_handler(event, context):
-    cityName = event['queryStringParameters']['cityName']
 
+def lambda_handler(event, context):
+    print('event:', json.dumps(event))
+
+    cityName = event['cityName']
+    print(event)
+    #cityName = 'madrid'
     city = "https://v1.nocodeapi.com/lmunoz/ow/gypnqySHxLOMWguT/byCityName?q="+cityName
 
-    print(city)
+    #print(city)
     response = requests.get(city)
     result = response.json()
+    
+    cityResponse = {}
+    cityResponse['cityName'] = cityName
+    
 
     responseObjet = {}
-    responseObjet['message'] = {'Gracias por utilizar la API'}
-
+    #responseObjet['message'] = {'Gracias por utilizar la API'}
     responseObjet['statusCode'] = 200
-    responseObjet['Headers'] {}
+    #responseObjet['headers'] {}
     responseObjet['body'] = result
 
     return responseObjet
-
-
-
+    #return json.dumps(event)
