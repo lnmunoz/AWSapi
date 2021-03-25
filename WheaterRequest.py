ec0fa29cd843b8c34ec756f4cd9aef5e1083f81d
lnmunoz <luchomunozm90@gmail.com>
2021-03-25 00:31:12 -0300

    Created local .py request

---
+++ WheaterRequest.py
@@ -0,0 +1,28 @@
+import requests
+import json
+print("Bienvenido a consultor de tiempo")
+ciudad = input("Ingrese nombre de ciudad: ")
+
+url = "https://euic8aetta.execute-api.us-east-1.amazonaws.com/dev/LambdaClima/crea"
+
+
+
+payload= '{\r\n    \"ciudad\":\"'+ciudad+'\"\r\n    }'
+
+
+#payload="{\r\n    \"ciudad\":\"cordoba\"\r\n    }"
+
+#print(pay)
+
+headers = {
+  'Content-Type': 'text/plain'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+salida = response.json()
+print("Datos del tiempo en: "+ciudad+" son: ")
+print("Temperatura: "+str(salida['temperatura']))
+print("Viento: "+str(salida['viento']))
+print("Gracias por utilizar el servicio")
