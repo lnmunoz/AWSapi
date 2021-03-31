8a3d8a87ccf36f25c0399dc8e7578061c71f07a3
lmunoz <lmunoz@ascentio.com>
2021-03-31 09:56:23 -0300

    problems solved with strange letters

--- resourses/WeatherRequest.py
+++ resourses/WeatherRequest.py
@@ -1,13 +1,36 @@
 import requests
 import json
+import unicodedata
+
+
+def quitartil(text):
+
+    try:
+        text = unicode(text, 'utf-8')
+    except NameError: # unicode is a default on python 3 
+        pass
+
+    text = unicodedata.normalize('NFD', text)\
+           .encode('ascii', 'ignore')\
+           .decode("utf-8")
+    return str(text)
+
 print("Bienvenido a consultor de tiempo")
 ciudad = str(input("Ingrese nombre de ciudad: "))
+#print (ciudad)
+ciudad = quitartil(ciudad)
+#print(ciudad)
+#Funcion para normalizar el nombre de la ciudad y quitar tildes
+
+
+
+
 
 #ciudad = input("Ingrese nombre de ciudad: ")
 
 #url = "https://euic8aetta.execute-api.us-east-1.amazonaws.com/dev/LambdaClima/crea"
 
-url = "https://sddhex0a2j.execute-api.us-east-1.amazonaws.com/dev/LambdaClima/crea"
+url = "https://2fcs79r6k1.execute-api.us-east-1.amazonaws.com/dev/LambdaClima/crea"
 
 payload= '{\r\n    \"ciudad\":\"'+ciudad+'\"\r\n    }'
 
@@ -22,7 +45,7 @@ headers = {
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
-#print(response.text)
+#print(response)
 
 salida = response.json()
 #print (salida)
